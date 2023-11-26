3bec3eb5f4d87bb27aec5651baf8055ba3afb844
Happyashbunny <ashishjaison96@gmail.com>
2023-11-26 13:34:31 +0530

    made an all postspage functionality to delete btn

---
+++ scripts/postList.js
@@ -0,0 +1,5 @@
+function deleteButtonHandler(id) {
+    var elementToRemove = document.getElementById(id)
+    // document.getElementById('container').innerHTML 
+    elementToRemove.style.display = 'none';
+}
